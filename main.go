bcb0f2f09a61baf978efd383a93fab76c74fa40a
yufan <yufan@live.com>
2024-03-26 15:53:51 +0800

    WIP 我使用 activity 的 documentation 节点存储 JSON 格式的配置数据，camunda2flowable 可以读取同名目录下的 JSON 文件并插入对应 ID activity 的 documentation

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 	"github.com/dlclark/regexp2"
 	"io"
 	"os"
+	"path/filepath"
+	"slices"
+	"strings"
 )
 
 const (
 	Version = "0.0.1"
 
 	header = `<?xml version="1.0" encoding="UTF-8"?>
-<definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:flowable="http://flowable.org/bpmn" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" xmlns:design="http://flowable.org/design" typeLanguage="http://www.w3.org/2001/XMLSchema" expressionLanguage="http://www.w3.org/1999/XPath" targetNamespace="http://flowable.org/test" design:palette="flowable-work-process-palette">
-`
+<definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:flowable="http://flowable.org/bpmn" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" xmlns:design="http://flowable.org/design" typeLanguage="http://www.w3.org/2001/XMLSchema" expressionLanguage="http://www.w3.org/1999/XPath" targetNamespace="http://flowable.org/test" design:palette="flowable-work-process-palette">`
 	footer = `
 </definitions>`
 )
@@ -50,6 +52,36 @@ func main() {
 	fMessages := ConvertMessages(cDefinitions.Message)
 	fMessagesInXML, _ := xml.MarshalIndent(fMessages, "  ", "  ")
 	fProcess := ConvertProcess(cDefinitions.Process)
+
+	definitionDir := strings.TrimSuffix(*source, filepath.Ext(*source))
+	//fileName := strings.TrimSuffix(filepath.Base(*source), filepath.Ext(*source))
+	// if same name directory exists
+	if _, err := os.Stat(definitionDir); !os.IsNotExist(err) {
+		activitieyIds := make([]string, 0)
+		files, _ := os.ReadDir(definitionDir)
+		for _, f := range files {
+			if strings.HasSuffix(f.Name(), ".json") {
+				// if f is JSON file, add to array
+				activityId := strings.TrimSuffix(f.Name(), filepath.Ext(f.Name()))
+				activitieyIds = append(activitieyIds, activityId)
+			}
+		}
+		if len(activitieyIds) != 0 {
+			for _, event := range fProcess.StartEvents {
+				if slices.Contains(activitieyIds, event.Id) {
+					documentation, _ := os.ReadFile(filepath.Join(definitionDir, event.Id+".json"))
+					event.Documentation.Value = string(documentation)
+				}
+			}
+			for _, task := range fProcess.UserTasks {
+				if slices.Contains(activitieyIds, task.Id) {
+					documentation, _ := os.ReadFile(filepath.Join(definitionDir, task.Id+".json"))
+					task.Documentation.Value = string(documentation)
+				}
+			}
+		}
+	}
+
 	fProcessInXML, _ := xml.MarshalIndent(fProcess, "  ", "  ")
 
 	// make tag self-closable
